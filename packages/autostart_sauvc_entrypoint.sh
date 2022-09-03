b9392d291a65ce9a464bebc9af7284dde09cfcf6
Vladislav Plotnikov <vladik1209@gmail.com>
2022-09-03 18:01:14 +0300

    add parameters

--- packages/autostart_sauvc_entrypoint.sh
+++ packages/autostart_sauvc_entrypoint.sh
@@ -4,12 +4,16 @@ set -e
 additional_packages_env_setup="/ws/additional_packages_ws/devel/setup.bash"
 echo "sourcing   $additional_packages_env_setup"
 source "$additional_packages_env_setup"
-sauvc_env_setup="/sauvc/devel/setup.bash"
-echo "sourcing   $sauvc_env_setup"
-source "$sauvc_env_setup"
+
 
 echo "ROS_ROOT   $ROS_ROOT"
 echo "ROS_DISTRO $ROS_DISTRO"
 
-roslaunch sauvc_startup sauvc_master.launch qr_launch:=true
+cd sauvc && catkin_make
+
+sauvc_env_setup="/sauvc/devel/setup.bash"
+echo "sourcing   $sauvc_env_setup"
+source "$sauvc_env_setup"
+
+roslaunch sauvc_startup main.launch QR_LAUNCH:=true STREAM:=TRUE DEBUG:=TRUE HARDWARE_CONNECTION:=TRUE
 
