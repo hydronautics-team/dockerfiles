b9d0f5adababfd83641d1878691da86fbc483ceb
Vladislav Plotnikov <vladik1209@gmail.com>
2022-09-03 14:45:42 +0300

    container name and cameras by path

--- run_sauvc_autostart.sh
+++ run_sauvc_autostart.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 docker run -dit --restart unless-stopped \
+    --name="sauvc_container" \
     -v /home/hydro/sauvc:/sauvc \
     -v /home/hydro/stingray_video_records:/root/stingray_video_records \
+    -v /dev/v4l/by-path:/dev/v4l/by-path \
     --env="ROS_IP=172.17.0.1" \
     --env="ROS_MASTER_URI=http://172.17.0.1:11311" \
     --net=host \
